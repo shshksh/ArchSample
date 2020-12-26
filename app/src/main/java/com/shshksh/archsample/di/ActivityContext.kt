6389473d4f09033d0e2759e1c77c72b637b0fb2e
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2020-12-26 22:39:04 +0900

    [DI] Define custom scope and qualifier.
    RetentionPolicy를 AnnotationRetention으로,
    @interface를 annotatino class로 변경.

---
+++ app/src/main/java/com/shshksh/archsample/di/ActivityContext.kt
@@ -0,0 +1,8 @@
+package com.shshksh.archsample.di
+
+import javax.inject.Qualifier
+
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityContext
---
+++ app/src/main/java/com/shshksh/archsample/di/ActivityScope.kt
@@ -0,0 +1,7 @@
+package com.shshksh.archsample.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
